de54129ed283935e6a7349ece70d061ac503f25e
Jinseon Lee <jjss5581@dgist.ac.kr>
2021-01-26 19:45:27 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Solve from "./pages/Solve";
 import Mentoring from "./pages/Mentoring";
 import ChatWithAdmin from "./pages/ChatWithAdmin";
